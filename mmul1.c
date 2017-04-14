8318eb79a7b81d945fe0e46ff6759b53449978de
Nick Liu <nickliuyanzhe@gmail.com>
2017-04-14 15:53:45 -0700

    part1 loop interchange

--- mmul1.c
+++ mmul1.c
@@ -1,15 +1,19 @@
 #include "const.h"
+#include <omp.h>
 
 void mmul1(float A[ni][nk], float B[nk][nj], float C[ni][nj])
-{
+{	
 	int i, j, k;
+	#pragma omp parallel private(i, j, k) shared(A, B, C)
+	{
+	#pragma omp for schedule(static)
 	for (i=0; i<ni; i++) {
-		for (j=0; j<nj; j++) {
-			C[i][j] = 0;
-			for (k=0; k<nk; k++) {
+		for (k=0; k<nk; k++) {
+			for (j=0; j<nj; j++) {
 				C[i][j] += A[i][k]*B[k][j];
 			}
 		}
+	}
 	}
 }
 
