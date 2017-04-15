9026a9d55f2aee291edc4b21aa91baa40878e11a
Nick Liu <nickliuyanzhe@gmail.com>
2017-04-14 22:01:42 -0700

    WIP

--- mmul2.c
+++ mmul2.c
@@ -1,12 +1,17 @@
 #include "const.h"
+#include "stdio.h"
+#include "string.h"
 
 void mmul2(float A[ni][nk], float B[nk][nj], float C[ni][nj])
 {
 	int i, j, k;
+	// prefill Matrix C with 0s
+	memset(C, 0, sizeof(C[0][0]) * ni * nj);
+	// create blocks for improvement
+	int _block_size = 16;
 	for (i=0; i<ni; i++) {
-		for (j=0; j<nj; j++) {
-			C[i][j] = 0;
-			for (k=0; k<nk; k++) {
+		for (k=0; k<nk; k++) {
+			for (j=0; j<nj; j++) {
 				C[i][j] += A[i][k]*B[k][j];
 			}
 		}
