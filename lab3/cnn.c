372afa86258379bdf7eb5a4d6fc827d0760d9010
Nick Liu <nickliuyanzhe@gmail.com>
2017-05-16 14:17:13 -0700

    Added boilerplate code

--- lab3/cnn.c
+++ lab3/cnn.c
@@ -3,7 +3,12 @@
 #include <time.h>
 #include <math.h>
 #include <sys/time.h>
+#include <string.h>
 #include "cnn.h"
+
+// OpenCL includes
+#include <CL/cl.h>
+
 
 // Sequential CNN implementation
 void conv(float Cout[NUM][OUTIMROW][OUTIMROW], float Cin[NUM][INIMROW][INIMROW],
@@ -55,7 +60,12 @@ void conv(float Cout[NUM][OUTIMROW][OUTIMROW], float Cin[NUM][INIMROW][INIMROW],
 	}
 }
 
-
+inline void checkErr(cl_int err, const char * name) {
+	if (err != CL_SUCCESS) {
+		fprintf(stderr, "ERROR: %s (%d)\n", name, err);
+		exit(EXIT_FAILURE);
+	}
+}
 int main()
 {
 	static float Cout[NUM][OUTIMROW][OUTIMROW];
@@ -70,7 +80,81 @@ int main()
 	struct timeval t1, t2;
 	gettimeofday(&t1, NULL);
 	// --- Please add your code below ---
-	conv(Cout, Cin, weight, bias);	
+	// conv(Cout, Cin, weight, bias);	
+	
+	// use this to check the output of each API call
+	cl_int status;
+	
+	/************************** PLATFORM **********************************/
+	// retrieve the number of platforms
+	cl_uint numPlatforms = 0;
+	status = clGetPlatformIDs(0, NULL, &numPlatforms);
+	checkErr(status, "Retrieve the number of platforms");
+	printf("Num of Platforms: %d\n", numPlatforms);
+	
+	// Allocate enough space for each platform
+	cl_platform_id *platforms = NULL;
+	platforms = (cl_platform_id*)malloc(numPlatforms * sizeof(cl_platform_id));
+
+	// Fill in the platforms
+	status = clGetPlatformIDs(numPlatforms, platforms, NULL);
+	checkErr(status, "Fill in the platforms");
+
+	// Find CPU
+	int platform_index = -1;
+	int i;
+	for (i = 0; i < numPlatforms; i++) 
+	{
+		char vendor[128];
+		clGetPlatformInfo(platforms[i], CL_PLATFORM_VENDOR, sizeof(vendor), vendor, NULL);
+		char vendorF[7];
+		memcpy((void*)vendorF, (void*)vendor, 6);
+		vendorF[6] = '\0';
+		fprintf(stderr, "%s\n", vendorF);
+		if (strcmp(vendorF, "Intel(") == 0)
+		{
+			platform_index = i;
+			break;
+		}
+
+	}
+	if (platform_index == -1) {
+		printf("CPU Platform not found!\n");
+		exit(1);
+	}
+
+	/***************************** DEVICE *******************************/
+
+	// Retrieve the number of devices
+	cl_uint numDevices = 0;
+	status = clGetDeviceIDs(platforms[platform_index], CL_DEVICE_TYPE_ALL, 0, NULL, &numDevices);
+	checkErr(status, "Retrieve the number of devices");
+	printf("Number of devices: %d, status: %d\n", numDevices, status);
+
+	// Allocate enough space for each device
+	cl_device_id *devices;
+	devices = (cl_device_id*) malloc(numDevices * sizeof(cl_device_id));
+
+	// Fill in the devices
+	status = clGetDeviceIDs(platforms[platform_index], CL_DEVICE_TYPE_ALL, numDevices, devices, NULL);
+	checkErr(status, "Fill in the devices");
+
+	/**************************** CONTEXT *******************************/
+
+	// create a context and associate it with the devices
+	cl_context context;
+	context = clCreateContext(NULL, numDevices, devices, NULL, NULL, &status);
+
+	/**************************** COMMAND QUEUE *************************/
+	
+	// create a command queue and associate it with the devices
+	cl_command_queue cmdQueue;
+	cmdQueue = clCreateCommandQueue(context, devices[0], 0, &status);
+
+	/***************************** BUFFER ******************************/
+	// create a buffer object that will contain the data from the host
+	//
+	
 
 	gettimeofday(&t2, NULL);
 	float elapsed_time = (t2.tv_sec - t1.tv_sec) + (t2.tv_usec - t1.tv_usec) / 1e6;
