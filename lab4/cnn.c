e70bd18d2003b9a4f310ad89a4f11674e7a9a7ac
Yanzhe Liu <nickliuyanzhe@gmail.com>
2017-05-21 17:34:43 -0700

    Copied lab3 code

--- lab4/cnn.c
+++ lab4/cnn.c
@@ -37,7 +37,7 @@ void conv(float Cout[NUM][OUTIMROW][OUTIMROW], float Cin[NUM][INIMROW][INIMROW],
 		for (int h = 0; h < IMROW; h++) {
 			for (int w = 0; w < IMROW; w++) {
 				C[i][h][w] = fmax(0, C[i][h][w]);
-			}	
+			}
 		}
 	}
 
@@ -62,7 +62,208 @@ void opencl_gpu(
    float bias[NUM]
 ){
 // please add your OpenCL setup code below
+struct timeval t1, t2;
+float elapsed_time;
 
+static float C[NUM][IMROW][IMROW];
+
+for(int i = 0; i < NUM; i++) {
+    for(int h = 0; h < IMROW; h++) {
+        for(int w = 0; w < IMROW; w++)
+            C[i][h][w] = bias[i];
+    }
+}
+
+// Convolution is parallelized because it is the bottle neck of the computation speed
+// use this to check the output of each API call
+cl_int status;
+
+/************************** PLATFORM **********************************/
+// retrieve the number of platforms
+cl_uint numPlatforms = 0;
+status = clGetPlatformIDs(0, NULL, &numPlatforms);
+checkErr(status, "Retrieve the number of platforms");
+printf("Num of Platforms: %d\n", numPlatforms);
+
+// Allocate enough space for each platform
+cl_platform_id *platforms = NULL;
+platforms = (cl_platform_id*)malloc(numPlatforms * sizeof(cl_platform_id));
+
+// Fill in the platforms
+status = clGetPlatformIDs(numPlatforms, platforms, NULL);
+checkErr(status, "Fill in the platforms");
+
+// Find CPU
+int platform_index = -1;
+int i;
+for (i = 0; i < numPlatforms; i++)
+{
+    char vendor[128];
+    clGetPlatformInfo(platforms[i], CL_PLATFORM_VENDOR, sizeof(vendor), vendor, NULL);
+    char vendorF[7];
+    memcpy((void*)vendorF, (void*)vendor, 6);
+    vendorF[6] = '\0';
+    fprintf(stderr, "%s\n", vendorF);
+    if (strcmp(vendorF, "Intel(") == 0)
+    {
+        platform_index = i;
+        break;
+    }
+
+}
+if (platform_index == -1) {
+    printf("CPU Platform not found!\n");
+    exit(1);
+}
+
+/***************************** DEVICE *******************************/
+
+// Retrieve the number of devices
+cl_uint numDevices = 0;
+status = clGetDeviceIDs(platforms[platform_index], CL_DEVICE_TYPE_ALL, 0, NULL, &numDevices);
+checkErr(status, "Retrieve the number of devices");
+printf("Number of devices: %d, status: %d\n", numDevices, status);
+
+// Allocate enough space for each device
+cl_device_id *devices;
+devices = (cl_device_id*) malloc(numDevices * sizeof(cl_device_id));
+
+// Fill in the devices
+status = clGetDeviceIDs(platforms[platform_index], CL_DEVICE_TYPE_ALL, numDevices, devices, NULL);
+checkErr(status, "Fill in the devices");
+
+/**************************** CONTEXT *******************************/
+
+// create a context and associate it with the devices
+cl_context context;
+context = clCreateContext(NULL, numDevices, devices, NULL, NULL, &status);
+
+/**************************** COMMAND QUEUE *************************/
+
+// create a command queue and associate it with the devices
+cl_command_queue cmdQueue;
+cmdQueue = clCreateCommandQueue(context, devices[0], 0, &status);
+
+/***************************** BUFFER ******************************/
+// create a buffer object that will contain the data from the host
+// Cin
+cl_mem bufCin;
+bufCin = clCreateBuffer(context, CL_MEM_READ_ONLY, NUM*INIMROW*INIMROW*sizeof(float), NULL, &status);
+
+// Weight
+cl_mem bufW;
+bufW = clCreateBuffer(context, CL_MEM_READ_ONLY, NUM*NUM*KERNEL*KERNEL*sizeof(float), NULL, &status);
+
+// Cconv
+cl_mem bufCconv;
+bufCconv = clCreateBuffer(context, CL_MEM_READ_WRITE, NUM*IMROW*IMROW*sizeof(float), NULL, &status);
+
+/****************************** ENQUEUE ****************************/
+
+// Write input Cin to the device buffer bufCin
+status = clEnqueueWriteBuffer(cmdQueue, bufCin, CL_FALSE, 0, NUM*INIMROW*INIMROW*sizeof(float),
+    Cin, 0, NULL, NULL);
+checkErr(status, "Write buffer Cin");
+
+// Write input Weight to the device buffer bufW
+status = clEnqueueWriteBuffer(cmdQueue, bufW, CL_FALSE, 0, NUM*NUM*KERNEL*KERNEL*sizeof(float),
+    weight, 0, NULL, NULL);
+checkErr(status, "Write buffer weight");
+
+// Write conv buffer to the device buffer bufCconv
+status = clEnqueueWriteBuffer(cmdQueue, bufCconv, CL_FALSE, 0, NUM*IMROW*IMROW*sizeof(float),
+    C, 0, NULL, NULL);
+checkErr(status, "Write buffer Cconv");
+
+/****************************** PROGRAM ****************************/
+// Create a program with source code
+gettimeofday(&t1, NULL);
+
+cl_program program = clCreateProgramWithSource(context, 1, (const char**)&kernel_cl, NULL, &status);
+
+// Build the program for the device
+status = clBuildProgram(program, numDevices, devices, NULL, NULL, NULL);
+if (status == CL_BUILD_PROGRAM_FAILURE) {
+    size_t log_size;
+    clGetProgramBuildInfo(program, devices[0], CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
+    char *log = (char*)malloc(log_size);
+    clGetProgramBuildInfo(program, devices[0], CL_PROGRAM_BUILD_LOG, log_size, log, NULL);
+    fprintf(stderr, "%s\n", log);
+    exit(1);
+}
+
+gettimeofday(&t2, NULL);
+elapsed_time = (t2.tv_sec - t1.tv_sec) + (t2.tv_usec - t1.tv_usec) / 1e6;
+fprintf(stderr, "Compilation time(s): %f\n", elapsed_time);
+
+/***************************** KERNEL ************************************/
+// create the conv kernel
+cl_kernel kernel;
+kernel = clCreateKernel(program, "conv", &status);
+
+// Associate the input and output buffers with the kernel
+status = clSetKernelArg(kernel, 0, sizeof(cl_mem), &bufCin);
+checkErr(status, "Set Arg 0");
+status = clSetKernelArg(kernel, 1, sizeof(cl_mem), &bufW);
+checkErr(status, "Set Arg 1");
+status = clSetKernelArg(kernel, 2, sizeof(cl_mem), &bufCconv);
+checkErr(status, "Set Arg 2");
+
+/******************************** WORK SIZE *******************************/
+// Define an index space of work items for execution. A workgroup size (local work size)
+// is not required, but can be used.
+size_t globalWorkSize[1];
+
+// There are NUM work-items
+globalWorkSize[0] = NUM;
+
+/******************************** EXECUTION *********************************/
+gettimeofday(&t1, NULL);
+
+// Execute the kernel
+status = clEnqueueNDRangeKernel(cmdQueue, kernel, 1, NULL,
+    globalWorkSize, NULL, 0, NULL, NULL);
+checkErr(status, "Execute Kernel");
+
+// read the device output buffer
+clEnqueueReadBuffer(cmdQueue, bufCconv, CL_TRUE, 0, NUM*IMROW*IMROW*sizeof(float),
+    C, 0, NULL, NULL);
+
+gettimeofday(&t2, NULL);
+elapsed_time = (t2.tv_sec - t1.tv_sec) + (t2.tv_usec - t1.tv_usec) / 1e6;
+fprintf(stderr, "Convolution time(s): %f\n", elapsed_time);
+
+clReleaseKernel(kernel);
+clReleaseProgram(program);
+clReleaseCommandQueue(cmdQueue);
+clReleaseMemObject(bufCin);
+clReleaseMemObject(bufW);
+clReleaseMemObject(bufCconv);
+clReleaseContext(context);
+
+// end of convolution
+
+// ReLU
+for (int i = 0; i < NUM; i++) {
+    for (int h = 0; h < IMROW; h++) {
+        for (int w = 0; w < IMROW; w++) {
+            C[i][h][w] = fmax(0, C[i][h][w]);
+        }
+    }
+}
+
+// Max pooling
+for (int i = 0; i < NUM; i++) {
+    for (int h = 0; h < OUTIMROW; h++) {
+        for (int w = 0; w < OUTIMROW; w++) {
+            float local_max = C[i][2 * h][2 * w];
+            local_max = fmax(local_max, C[i][2 * h + 1][2 * w]);
+            local_max = fmax(local_max, C[i][2 * h + 1][2 * w + 1]);
+            local_max = fmax(local_max, C[i][2 * h][2 * w + 1]);
+            Cout[i][h][w] = local_max;
+        }
+    }
+}
 }
 
 int main(){
@@ -80,7 +281,7 @@ int main(){
 	// --- Please add OpenCL setup code inside the function below ---
    opencl_gpu(
       Cout, Cin, weight, bias
-   );	
+   );
 
    // --- Timing stuff
 	gettimeofday(&t2, NULL);
---
+++ lab4/kernel.cl
@@ -0,0 +1,42 @@
+#define NUM 256
+#define INIMROW 228
+#define IMROW 224
+#define OUTIMROW 112
+#define KERNEL 5
+#define NKK 6400
+#define IMROW2 50176
+#define KK 25
+#define INIMROW2 51984
+
+__kernel
+void conv(	__global float *Cin,
+		__global float *weight,
+		__global float *Cconv)
+{
+   	// Get the work-item's unique ID
+   	int i = get_global_id(0);
+
+	// Convolution
+	for(int j = 0; j < NUM; j++) {
+		// local buffer for w[i][j]
+		__private float w_local[KERNEL][KERNEL];
+		for (int p = 0; p < KERNEL; p++) {
+			for (int q = 0; q < KERNEL; q++) {
+				w_local[p][q] = weight[i*NKK + j*KK + p*KERNEL + q];
+			}
+		}
+		for(int h = 0; h < IMROW; h++) {
+			for(int w = 0; w < IMROW; w+=4) {
+				__private float4 tmp4;
+				tmp4 = vload4(0, &Cconv[i*IMROW2 + h*IMROW + w]);
+				for(int p = 0; p < KERNEL; p++) {
+					for(int q = 0; q < KERNEL; q++) {
+						float4 i4 = vload4(0, &Cin[j*INIMROW2 + (h + p) * INIMROW + w + q]);
+						tmp4 = tmp4 + i4 * w_local[p][q];
+					}
+				}
+				vstore4(tmp4, 0, &Cconv[i*IMROW2 + h*IMROW + w]);
+			}
+		}
+	}
+}
