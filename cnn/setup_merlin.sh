d0e7dbf74d1b6aca653edacb60193482a86bc302
Nick Liu <nickliuyanzhe@gmail.com>
2017-06-06 03:07:52 -0700

    Remove previous folder

--- cnn/setup_merlin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /usr/local/cs133/FCS/Merlin_Compiler_2017.1/settings64.sh 
-export LM_LICENSE_FILE=/usr/local/cs133/FCS/license/falcon_cs133.lic
-
-lic_file="xilinx_$( cut -d '.' -f 1 <<< `hostname` ).lic"
-export LM_LICENSE_FILE=/usr/local/cs133/Xilinx/SDx/license/${lic_file}:${LM_LICENSE_FILE}
-
